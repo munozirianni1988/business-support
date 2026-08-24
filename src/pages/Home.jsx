7b3b49035a43b7f21f714eef09a76859a55db4d2
munozirianni1988 <glenirianni@gmail.com>
2026-08-24 21:32:56 +0100

    Building website skeleton.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
+
+export default Home;
